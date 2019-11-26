a396feb5a9a8f3b27ec2091852c723bc1362ada0
Jonathan Buzzetti <buzzetti.jp@gmail.com>
2019-11-26 09:33:25 -0300

    Hola Mundo con scanner

--- src/main/java/com/crediloco/crediloco/Main.java
+++ src/main/java/com/crediloco/crediloco/Main.java
@@ -1,10 +1,17 @@
 package com.crediloco.crediloco;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] parametros) {
-        System.out.println("Hola mundo!");
+        System.out.println("Cómo te llamas?");
+        Scanner scanner = new Scanner(System.in);  // Crear un scanner
+
+        String userName = scanner.nextLine();  // Leer entrada de usuario
+
+        System.out.println("Hola " + userName + "!");
         
     }
-    
+
 }
